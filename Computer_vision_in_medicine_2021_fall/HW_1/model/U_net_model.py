6a51c19ebcad79e4425f6c7e6fab249532e7c605
pavelg0t <pavelgto@gmail.com>
2021-10-19 17:28:58 +0300

    Update U_net_model.py

--- Computer_vision_in_medicine_2021_fall/HW_1/model/U_net_model.py
+++ Computer_vision_in_medicine_2021_fall/HW_1/model/U_net_model.py
@@ -3,55 +3,56 @@ from tensorflow.keras.layers import Layer, InputSpec, Input, Conv2D, Lambda, Max
 from tensorflow.keras import Model
 
 
-class ReflectionPadding2D(Layer):
-    '''
-    Adding reflected padding before Conv2D() layer
-    '''
-    def __init__(self, padding=(1, 1), **kwargs):
-        self.padding = tuple(padding)
-        self.input_spec = [InputSpec(ndim=4)]
-        super(ReflectionPadding2D, self).__init__(**kwargs)
+# class ReflectionPadding2D(Layer):
+#     '''
+#     Adding reflected padding before Conv2D() layer
+#     '''
+#     def __init__(self, padding=(1, 1), **kwargs):
+#         self.padding = tuple(padding)
+#         self.input_spec = [InputSpec(ndim=4)]
+#         super(ReflectionPadding2D, self).__init__(**kwargs)
 
-    def compute_output_shape(self, s):
-        """ If you are using "channels_last" configuration"""
-        return (s[0], s[1] + 2 * self.padding[0], s[2] + 2 * self.padding[1], s[3])
+#     def compute_output_shape(self, s):
+#         """ If you are using "channels_last" configuration"""
+#         return (s[0], s[1] + 2 * self.padding[0], s[2] + 2 * self.padding[1], s[3])
 
-    def call(self, x, mask=None):
-        w_pad,h_pad = self.padding
-        return tf.pad(x, [[0,0], [h_pad,h_pad], [w_pad,w_pad], [0,0] ], 'REFLECT')
+#     def call(self, x, mask=None):
+#         w_pad,h_pad = self.padding
+#         return tf.pad(x, [[0,0], [h_pad,h_pad], [w_pad,w_pad], [0,0] ], 'REFLECT')
     
-    # For purooses as stated in: https://stackoverflow.com/questions/58678836/notimplementederror-layers-with-arguments-in-init-must-override-get-conf
-    # def get_config(self):
+#     # For purooses as stated in: https://stackoverflow.com/questions/58678836/notimplementederror-layers-with-arguments-in-init-must-override-get-conf
+#     # def get_config(self):
 
-    #     config = super().get_config().copy()
-    #     config.update({
-    #         'padding': self.vocab_size,
-    #         'input_spec': self.input_spec
-    #     })
-    #     return config
+#     #     config = super().get_config().copy()
+#     #     config.update({
+#     #         'padding': self.vocab_size,
+#     #         'input_spec': self.input_spec
+#     #     })
+#     #     return config
     
     
-    # For purooses as stated in:  https://keras.io/guides/making_new_layers_and_models_via_subclassing/
-    def get_config(self):
-        config = super(ReflectionPadding2D, self).get_config()
-        config.update({"padding": self.padding, "input_spec": self.input_spec})
-        return config
+#     # For purooses as stated in:  https://keras.io/guides/making_new_layers_and_models_via_subclassing/
+#     def get_config(self):
+#         config = super(ReflectionPadding2D, self).get_config()
+#         config.update({"padding": self.padding, "input_spec": self.input_spec})
+#         return config
     
-    @classmethod
-    def from_config(cls, config):
-      """Creates a layer from its config.
-      This method is the reverse of `get_config`,
-      capable of instantiating the same layer from the config
-      dictionary. It does not handle layer connectivity
-      (handled by Network), nor weights (handled by `set_weights`).
-      Arguments:
-          config: A Python dictionary, typically the
-              output of get_config.
-      Returns:
-          A layer instance.
-      """
-      return cls(**config)
     
+#     @classmethod
+#     def from_config(cls, config):
+#       """Creates a layer from its config.
+#       This method is the reverse of `get_config`,
+#       capable of instantiating the same layer from the config
+#       dictionary. It does not handle layer connectivity
+#       (handled by Network), nor weights (handled by `set_weights`).
+#       Arguments:
+#           config: A Python dictionary, typically the
+#               output of get_config.
+#       Returns:
+#           A layer instance.
+#       """
+#       return cls(**config)
+
 
 def get_model(WIDTH,HEIGHT,INPT_CHANNELS,N_ch=16):
 
@@ -66,72 +67,72 @@ def get_model(WIDTH,HEIGHT,INPT_CHANNELS,N_ch=16):
   
 
   #Contraction path (Encoder) 
-  c1 = ReflectionPadding2D(padding=(1,1))(input)
+  c1 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(input)
   c1 = Conv2D(filters=N_ch, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c1)
   c1 = BatchNormalization()(c1)  
-  c1 = ReflectionPadding2D(padding=(1,1))(c1)
+  c1 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c1)
   c1 = Conv2D(filters=N_ch, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c1)
   p1 = MaxPool2D(pool_size=(2, 2))(c1)
 
-  c2 = ReflectionPadding2D(padding=(1,1))(p1)
+  c2 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(p1)
   c2 = Conv2D(filters=N_ch*2, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c2)
   c2 = BatchNormalization()(c2)  
-  c2 = ReflectionPadding2D(padding=(1,1))(c2)
+  c2 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c2)
   c2 = Conv2D(filters=N_ch*2, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c2)
   p2 = MaxPool2D(pool_size=(2, 2))(c2)
 
-  c3 = ReflectionPadding2D(padding=(1,1))(p2)
+  c3 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(p2)
   c3 = Conv2D(filters=N_ch*4, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c3)
   c3 = BatchNormalization()(c3) 
-  c3 = ReflectionPadding2D(padding=(1,1))(c3)
+  c3 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c3)
   c3 = Conv2D(filters=N_ch*4, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c3)
   p3 = MaxPool2D(pool_size=(2, 2))(c3)
 
-  c4 = ReflectionPadding2D(padding=(1,1))(p3)
+  c4 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(p3)
   c4 = Conv2D(filters=N_ch*8, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c4)
   c4 = BatchNormalization()(c4) 
-  c4 = ReflectionPadding2D(padding=(1,1))(c4)
+  c4 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c4)
   c4 = Conv2D(filters=N_ch*8, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c4)
   p4 = MaxPool2D(pool_size=(2, 2))(c4)
 
-  c5 = ReflectionPadding2D(padding=(1,1))(p4)
+  c5 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(p4)
   c5 = Conv2D(filters=N_ch*16, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c5)
   c5 = BatchNormalization()(c5)  
-  c5 = ReflectionPadding2D(padding=(1,1))(c5)
+  c5 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c5)
   c5 = Conv2D(filters=N_ch*16, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c5)
 
 
   #Expansion path (Decoder)
   u6 = Conv2DTranspose(filters=N_ch*8, kernel_size=(2,2), strides=(2, 2), padding="same")(c5)
   u6 = Concatenate()([u6,c4])
-  c6 = ReflectionPadding2D(padding=(1,1))(u6)
+  c6 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(u6)
   c6 = Conv2D(filters=N_ch*8, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c6)
   c6 = BatchNormalization()(c6)  
-  c6 = ReflectionPadding2D(padding=(1,1))(c6)
+  c6 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c6)
   c6 = Conv2D(filters=N_ch*8, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c6)
 
   u7 = Conv2DTranspose(filters=N_ch*4, kernel_size=(2,2), strides=(2, 2), padding="same")(c6)
   u7 = Concatenate()([u7,c3])
-  c7 = ReflectionPadding2D(padding=(1,1))(u7)
-  c7 = Conv2D(filters=N_ch*4, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c7)
+  c7 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(u7)
+  c7 = Conv2D(filters=N_ch*4, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(u7)
   c7 = BatchNormalization()(c7)  
-  c7 = ReflectionPadding2D(padding=(1,1))(c7)
+  c7 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c7)
   c7 = Conv2D(filters=N_ch*4, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c7)
 
   u8 = Conv2DTranspose(filters=N_ch*2, kernel_size=(2,2), strides=(2, 2), padding="same")(c7)
   u8 = Concatenate()([u8,c2])
-  c8 = ReflectionPadding2D(padding=(1,1))(u8)
-  c8 = Conv2D(filters=N_ch*2, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c8)
+  c8 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(u8)
+  c8 = Conv2D(filters=N_ch*2, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(u8)
   c8 = BatchNormalization()(c8)  
-  c8 = ReflectionPadding2D(padding=(1,1))(c8)
+  c8 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c8)
   c8 = Conv2D(filters=N_ch*2, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c8)
 
   u9 = Conv2DTranspose(filters=N_ch, kernel_size=(2,2), strides=(2, 2), padding="same")(c8)
   u9 = Concatenate()([u9,c1])
-  c9 = ReflectionPadding2D(padding=(1,1))(u9)
-  c9 = Conv2D(filters=N_ch, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c9)
+  c9 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(u9)
+  c9 = Conv2D(filters=N_ch, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(u9)
   c9 = BatchNormalization()(c9)  
-  c9 = ReflectionPadding2D(padding=(1,1))(c9)
+  c9 = Lambda(lambda x: tf.pad(x, [[0,0], [1,1], [1,1], [0,0] ], 'REFLECT'))(c9)
   c9 = Conv2D(filters=N_ch, kernel_size=(3,3), activation='relu', kernel_initializer="he_normal", padding='valid')(c9)
 
   output = Conv2D(filters=1, kernel_size=(1,1), activation='sigmoid',padding="same")(c9)
@@ -139,3 +140,6 @@ def get_model(WIDTH,HEIGHT,INPT_CHANNELS,N_ch=16):
   model = Model(inputs=input, outputs=output)
 
   return model
+
+
+model = get_model(WIDTH=128,HEIGHT=128,INPT_CHANNELS=3,N_ch=16)
