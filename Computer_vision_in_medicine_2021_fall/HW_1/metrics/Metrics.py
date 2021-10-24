f2a4304d5af33154431b86b907cdc2dad21679fd
pavelg0t <pavelgto@gmail.com>
2021-10-24 13:23:45 +0300

    Update Metrics.py

--- Computer_vision_in_medicine_2021_fall/HW_1/metrics/Metrics.py
+++ Computer_vision_in_medicine_2021_fall/HW_1/metrics/Metrics.py
@@ -3,41 +3,49 @@ import numpy as np
 import tensorflow as tf
 
 def get_Recall(y_truth, y_pred):
-  y_pred = y_pred >0.5
-  y_pred = y_pred.astype(int)
-  by_class = recall_score(y_truth, y_pred, average=None)
-  mean = by_class.mean()
-  weighted = recall_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
-  res.update({"metric": "Recall", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
-  return res
+    y_pred = y_pred >0.5
+    y_pred = y_pred.astype(int)
+    by_class = recall_score(y_truth, y_pred, average=None)
+    mean = by_class.mean()
+    weighted = recall_score(y_truth, y_pred, average='weighted')
+    res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+    res.update({"metric": "Recall", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
+    return res
 
 def get_Precision(y_truth, y_pred):
-  y_pred = y_pred >0.5
-  y_pred = y_pred.astype(int)
-  by_class = precision_score(y_truth, y_pred, average=None)
-  mean = by_class.mean()
-  weighted = precision_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
-  res.update({"metric": "Precision", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
-  return res
+    y_pred = y_pred >0.5
+    y_pred = y_pred.astype(int)
+    by_class = precision_score(y_truth, y_pred, average=None)
+    mean = by_class.mean()
+    weighted = precision_score(y_truth, y_pred, average='weighted')
+    res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+    res.update({"metric": "Precision", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
+    return res
 
 def get_F1(y_truth, y_pred):
-  y_pred = y_pred >0.5
-  y_pred = y_pred.astype(int)
-  by_class = f1_score(y_truth, y_pred, average=None)
-  mean = by_class.mean()
-  weighted = f1_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
-  res.update({"metric": "F1", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
-  return res
+    y_pred = y_pred >0.5
+    y_pred = y_pred.astype(int)
+    by_class = f1_score(y_truth, y_pred, average=None)
+    mean = by_class.mean()
+    weighted = f1_score(y_truth, y_pred, average='weighted')
+    res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+    res.update({"metric": "F1", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
+    return res
 
 def get_AUC(y_truth, y_pred):
-  m = tf.keras.metrics.AUC()
-  m.update_state(y_truth, y_pred)
-  return m.result().numpy()
+    m = tf.keras.metrics.AUC()
+    m.update_state(y_truth, y_pred)
+    return m.result().numpy()
 
-def get_mIoU(y_truth, y_pred):
-  m = tf.keras.metrics.MeanIoU(num_classes=2)
-  m.update_state(y_truth, y_pred)
-  return m.result().numpy()
+
+def get_IoU(y_truth, y_pred):
+    '''
+    y_truth: 1D int vector
+    y_pred: 1D int vector
+    '''
+    by_class = jaccard_score(y_truth, y_pred, average=None)
+    mean = by_class.mean()
+    weighted = jaccard_score(y_truth, y_pred, average='weighted')
+    res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+    res.update({"metric": "IoU", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
+    return res
