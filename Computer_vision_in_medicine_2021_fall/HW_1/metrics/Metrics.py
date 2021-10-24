b4b4b64ba1ee9998cf35507ce3d19c8f5e07bb66
pavelg0t <pavelgto@gmail.com>
2021-10-24 12:47:38 +0300

    Update Metrics.py

--- Computer_vision_in_medicine_2021_fall/HW_1/metrics/Metrics.py
+++ Computer_vision_in_medicine_2021_fall/HW_1/metrics/Metrics.py
@@ -5,31 +5,31 @@ import tensorflow as tf
 def get_Recall(y_truth, y_pred):
   y_pred = y_pred >0.5
   y_pred = y_pred.astype(int)
-  by_class = np.round(recall_score(y_truth, y_pred, average=None),3)
+  by_class = recall_score(y_truth, y_pred, average=None)
   mean = by_class.mean()
   weighted = recall_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i): by_class[i] for i in range(by_class.size)} 
-  res.update({"metric": "Recall", "mean": np.round(mean,3), "weighted": np.round(weighted,3)})
+  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+  res.update({"metric": "Recall", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
   return res
 
 def get_Precision(y_truth, y_pred):
   y_pred = y_pred >0.5
   y_pred = y_pred.astype(int)
-  by_class = np.round(precision_score(y_truth, y_pred, average=None),3)
+  by_class = precision_score(y_truth, y_pred, average=None)
   mean = by_class.mean()
   weighted = precision_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i): by_class[i] for i in range(by_class.size)} 
-  res.update({"metric": "Precision", "mean": np.round(mean,3), "weighted": np.round(weighted,3)})
+  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+  res.update({"metric": "Precision", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
   return res
 
 def get_F1(y_truth, y_pred):
   y_pred = y_pred >0.5
   y_pred = y_pred.astype(int)
-  by_class = np.round(f1_score(y_truth, y_pred, average=None),3)
+  by_class = f1_score(y_truth, y_pred, average=None)
   mean = by_class.mean()
   weighted = f1_score(y_truth, y_pred, average='weighted')
-  res = { 'class ' + str(i): by_class[i] for i in range(by_class.size)} 
-  res.update({"metric": "F1", "mean": np.round(mean,3), "weighted": np.round(weighted,3)})
+  res = { 'class ' + str(i):  np.round(by_class[i],3).tolist() for i in range(by_class.size)} 
+  res.update({"metric": "F1", "mean": np.round(mean,3).tolist(), "weighted": np.round(weighted,3).tolist()})
   return res
 
 def get_AUC(y_truth, y_pred):
